ae4b30722dae8b68a47a4602ea686f33036a86b4
nuller-jo <nuller.jmain@gmail.com>
2018-12-12 21:29:39 +0900

    サンプルコード生成

--- routing.py
+++ routing.py
@@ -1,8 +1,7 @@
+# -*- coding: utf-8 -*-
+from hello.main import say_hello
 
-
-def message(message,client):
+def message(commandList,client,message):
     #ここにメッセージを解析して処理を呼び出す処理を書こう！！
-    # 送り主がBotだった場合反応しない
-        if client.user != message.author:
-            if message.content.startswith('mkvi'):
-                pass
+    if(commandList[1] == 'hello'):
+        say_hello(client,message)
